2b9dcf34e14e3f9e2e6714b884f3af84b4fa4c96
Alejandro G. Bologna <alejandrobologna2003@gmail.com>
2023-06-22 10:03:03 -0300

    1era Preentrega

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola, Coder!</Text>
+      <Text>1era Preentrega!</Text>
       <StatusBar style="auto" />
     </View>
   );
